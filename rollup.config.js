d23d6f06c61a483d9d8eea3fbc97a78791265996
Raz Konforti <konforti@gmail.com>
2018-07-23 22:44:56 +0300

    terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
     input: 'index.js',
     output: {
         file: 'dist/index.js',
         format: 'es',
     },
+    plugins: [terser()],
 };
